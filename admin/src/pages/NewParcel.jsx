5442e6b32a36ec137f939497b3d17e3e0d45849f
Believe Gilbert <believegilbert20@gmail.com>
2024-11-06 23:06:55 +0100

    created parcels and users page for the admin dashboard

--- admin/src/pages/NewParcel.jsx
+++ admin/src/pages/NewParcel.jsx
@@ -1,8 +1,12 @@
 
+
 const NewParcel = () => {
   return (
-    <div>
-      
+    <div className='m-[30px] bg-[#fff] p-[20px]'>
+      <h2 className="font-semibold">New Parcel</h2>
+      <div className="flex">
+
+      </div>
     </div>
   )
 }
--- admin/src/pages/Parcels.jsx
+++ admin/src/pages/Parcels.jsx
@@ -1,5 +1,6 @@
 import { DataGrid } from '@mui/x-data-grid';
 import { FaTrash } from 'react-icons/fa';
+import { Link } from 'react-router-dom'
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -47,7 +48,9 @@ const Parcels = () => {
     <div className="m-[30px] bg-[#fff] p-[20px]">
       <div className="flex items-center justify-between">
           <h1 className="m-[20px] text-[20px]">All parcels</h1>
+          <Link to="/newparcel">
           <button className="bg-[#1e1e1e] text-white p-[10px] cursor-pointer">New Parcel</button>
+          </Link>
       </div>
 
       <DataGrid 
