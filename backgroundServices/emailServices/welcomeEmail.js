0ce387d5abb9e73f8fc478b7054be7baf1514d20
Believe Gilbert <believegilbert20@gmail.com>
2024-11-24 18:35:43 +0100

    background service working fine

--- backgroundServices/emailServices/welcomeEmail.js
+++ backgroundServices/emailServices/welcomeEmail.js
@@ -4,41 +4,34 @@ dotenv.config();
 const User = require("../models/user");
 const sendMail = require("../helpers/sendmail");
 
-const sendWelcomeEmail = async () => {
-  try {
-    const users = await User.find({ status: 0 });
+const sendWelcomeEmail = async (fullname, password, email) => {
+  const users = await User.find({ status: 0 });
+ 
 
-    if (users.length > 0) {
-      for (let user of users) {
-        try {
-          //render the email template as a promise
-          const data = ejs.renderFile("templates/welcome.ejs", {
-            fullname: user.fullname,
-            email: user.email,
-            password: user.password1,
-          });
-
-          //set up the message options
-          const messageoption = {
+  if (users.length > 0) {
+    for (let user of users) {
+     
+      ejs.renderFile(
+        "templates/welcome.ejs",
+        { fullname: user.fullname,password:user.password1, email: user.email },
+        async (err, data) => {
+          let messageoption = {
             from: process.env.EMAIL,
             to: user.email,
             subject: "Welcome to SendIT",
             html: data,
           };
 
-          //send the email
-          await sendMail(messageoption);
-
-          //update the user status and clear the temporal password called password1
-          await User.findByIdAndUpdate(user._id, { $set: { status: 1 } });
-          await User.findByIdAndUpdate(user._id, { $set: { password1: "" } });
-        } catch (error) {
-          `error sending email to ${user.email}: ${error.message}`;
+          try {
+            sendMail(messageoption);
+            await User.findByIdAndUpdate(user._id, { $set: { status: 1 } });
+            await User.findByIdAndUpdate(user._id, { $set: { password1: "" } });
+          } catch (error) {
+            console.log(err);
+          }
         }
-      }
+      );
     }
-  } catch (error) {
-    console.log("error fetching users: ", error.message);
   }
 };
 
