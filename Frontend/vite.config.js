28b0df6ead6842955b1f2f925c8db6f38c55dd4c
Believe Gilbert <believegilbert20@gmail.com>
2025-01-01 23:52:10 +0100

    added rollup external build config to vite.config.js in the frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-base: "/"
+base: "/",
+build: {
+  rollupOptions: {
+    external: ['axios'], // Add axios to the external list
+  }
+}
 })
