60db7c3771073460aa78bb5066fc5d0f18282a13
Believe Gilbert <believegilbert20@gmail.com>
2025-01-05 11:20:13 +0100

    added build directory to the frontend vite config file

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,9 +6,7 @@ export default defineConfig({
   plugins: [react()],
 base: "/",
 build: {
-  rollupOptions: {
-    external: ['axios'], // Add axios to the external list
-  }
-}
+  outDir: 'dist', // Ensure the output directory is 'dist'
+},
 
 })
