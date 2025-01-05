d799f714b5c1e7fe054eafc8811c54a2092adebb
Believe Gilbert <believegilbert20@gmail.com>
2025-01-05 21:20:04 +0100

    checks

--- Frontend/src/requestMethods.js
+++ Frontend/src/requestMethods.js
@@ -1,9 +1,14 @@
-import axios from "redaxios"
+import axios from "axios"
 
 const BASE_URL =  "https://sendit1backend.onrender.com/api/v1"
 // const BASE_URL =  import.meta.env.backendApiUrl
 
 
 export const publicRequest = axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+    withCredentials: false,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    }
 })
